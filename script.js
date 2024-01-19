55775450895c2e7218426cdf5ce3ed1ed554d1f6
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-01-19 16:59:16 +0200

    added more sections, elements, more features

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
